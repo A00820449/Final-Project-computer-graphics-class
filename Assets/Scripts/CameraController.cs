67ad7ee5bb237bb38a2d65515c81521b2c1998a8
Miguel Tornero <a00820449@itesm.mx>
2022-07-27 13:12:05 -0500

    added camera rotation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float MOVEMENT_SPEED = 1f;
+    public float ROTATION_SPEED = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,14 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.position += this.transform.forward * Input.GetAxis("Vertical") * MOVEMENT_SPEED;
-        this.transform.position += this.transform.right * Input.GetAxis("Horizontal") * MOVEMENT_SPEED;
+        if (Input.GetMouseButton(0))
+        {
+            this.transform.eulerAngles += new Vector3(Input.GetAxis("Mouse Y"), -Input.GetAxis("Mouse X"), 0f) * ROTATION_SPEED;
+        }
+        else
+        {
+            this.transform.position += this.transform.forward * Input.GetAxis("Vertical") * MOVEMENT_SPEED;
+            this.transform.position += this.transform.right * Input.GetAxis("Horizontal") * MOVEMENT_SPEED;
+        }
     }
 }
